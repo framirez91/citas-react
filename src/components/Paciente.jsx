963bd95fa655c1bad412218e3e224fff96622607
Felipe Ramirez <80292125+framirez91@users.noreply.github.com>
2022-01-26 20:12:11 -0300

    creacion de botones eliminar y editar

--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,6 +1,6 @@
 const Paciente = ({ paciente }) => {
     const { nombre, propietario, email, fecha, sintomas } = paciente;
-   
+
     return (
         <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
             <p className="font-bold mb-3 text-gray-700 uppercase"> Nombre: {" "}
@@ -18,6 +18,19 @@ const Paciente = ({ paciente }) => {
             <p className="font-bold mb-3 text-gray-700 uppercase"> Sintomas: {" "}
                 <span className=" font-normal normal-case">{sintomas}</span>
             </p>
+            <div
+            className="flex justify-between mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white uppercase font-bold rounded-lg"
+                > Editar
+                </button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white uppercase font-bold rounded-lg"
+                > Eliminar
+                </button>
+            </div>
         </div>
     );
 };
