11b3c1f0dd40c1f24c01ec75ee68d6504e4f73bc
Felipe Ramirez <80292125+framirez91@users.noreply.github.com>
2022-01-26 22:28:02 -0300

    se agrega local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import Header from "./components/Header"
 import Formulario from "./components/Formulario"
-import ListadoPacientes from "./components/ListadoPacientes";
+import ListadoPacientes from "./components/ListadoPacientes"
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState([])
   const [paciente, setPaciente] = useState({})//cada paciente es un objeto por eso este inicia como un objeto vacio//genero la funcion aqui
+  
+  //el orden de los effect es como se ejectua el codigo
+  useEffect(() => {
+    const obtenerLS = () => {
+    const pacientesLS = JSON.parse(localStorage.getItem('pacientes'))??[]//detecta que hay algo en storage
+    setPacientes(pacientesLS)//lo coloca en el state
+  }  
+  obtenerLS()
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes))// lo convierte a string para que lo pueda guardar en el local storage
+
+  }, [pacientes])//aqui lo maniente en el state
   
   const eliminarPaciente = id => {
     const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id)//elimino el paciente que tenga el mismo id que el que se elimino
     setPacientes(pacientesActualizados)//actualizo el state
-  
   }
   
+
   return (
 
     <div className="container mx-auto mt-20">
